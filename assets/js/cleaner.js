6de497b4cd17b3340f4f3e609d21e6675be9032d
Mantra Ventures CTO <rico@moscovium.local>
2020-07-28 11:21:33 +0100

    cookie erase error fix

--- assets/js/cleaner.js
+++ assets/js/cleaner.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", function() {
   function eraseCookie(name) {   
-    document.cookie = name+'=; Max-Age=-99999999;';
+    document.cookie = name+'=; Max-Age=-99999999; SameSite=Lax';
   }
 
   eraseCookie('_ga');

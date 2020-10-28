74fbaa0051b8df9de711eac043ef4eaff7caa93e
Mantra Ventures CTO <rico@moscovium.local>
2020-10-28 14:09:23 +0000

    video embed optimisation

---
+++ docs/assets/js/video-embed.js
@@ -0,0 +1,30 @@
+/* Light YouTube Embeds by @labnol */
+/* Web: http://labnol.org/?p=27941 */
+
+document.addEventListener("DOMContentLoaded",
+  function() {
+    var div, n,
+        v = document.getElementsByClassName("youtube-player");
+    for (n = 0; n < v.length; n++) {
+      div = document.createElement("div");
+      div.setAttribute("data-id", v[n].dataset.id);
+      div.innerHTML = labnolThumb(v[n].dataset.id);
+      div.onclick = labnolEmbed;
+      v[n].appendChild(div);
+    }
+});
+
+function labnolThumb(id) {
+  var thumb = '<img src="https://i.ytimg.com/vi/ID/maxresdefault.jpg" loading="lazy">',
+    play = '<div class="play"></div>';
+  return thumb.replace("ID", id) + play;
+}
+
+function labnolEmbed() {
+  var embed = document.createElement("embed");
+  var url = "https://www.youtube.com/embed/ID?autoplay=1&amp;rel=0&amp;showinfo=0";
+  embed.setAttribute("src", url.replace("ID", this.dataset.id));
+  embed.setAttribute("frameborder", "0");
+  embed.setAttribute("allowfullscreen", "1");
+  this.parentNode.replaceChild(embed, this);
+}

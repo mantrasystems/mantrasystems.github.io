acd0bc9673fecef788c879acce522dce9b2fb8c0
Mantra Ventures CTO <rico@moscovium.local>
2021-10-06 16:42:45 +0100

    cleaner update

--- docs/assets/js/cleaner.js
+++ docs/assets/js/cleaner.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", function() {
   function eraseCookie(name) {   
-    document.cookie = name+'=; Max-Age=-99999999; SameSite=Lax';
+    document.cookie = name+'=; Domain=.mantrasystems.co.uk; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT; SameSite=Lax';
   }
 
   eraseCookie('_ga');

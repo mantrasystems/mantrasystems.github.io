1fd276be5f24c354446473b0bc4f8846287b3b11
rico <rico@MacBook-Pro.local>
2023-07-28 15:53:28 +0100

    landing pages + article page intro + email copy

---
+++ docs/assets/js/copy-email.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded",
+  function() {
+    var anchor, n,
+        v = document.getElementsByClassName("email-address");
+    for (n = 0; n < v.length; n++) {
+      var emailAddress = v[n].getAttribute("href").replace('mailto:','').replace(/(\?.*)|(#.*)/g,'');
+      /data-/g,''
+      anchor = document.createElement("a");
+      anchor.setAttribute("class", "copy-email icon-copy");
+      anchor.setAttribute("title", "Copy email address");
+      anchor.setAttribute("href", "#");
+      anchor.innerHTML = "<span>Copy email address</span>";
+
+      v[n].insertAdjacentElement("afterend", anchor);
+
+      anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        navigator.clipboard.writeText(emailAddress);
+      },);
+    }
+});
